05e5ef7b18a7b55690565e1caef40ed8774d2bd6
KipsangA <kipsang47@gmail.com>
2021-03-30 15:10:49 +0300

    first commit

---
+++ admin/inc/footer.php
@@ -0,0 +1,12 @@
+ <div class="clear">
+        </div>
+    </div>
+    <div class="clear">
+    </div>
+    <div id="site_info">
+        <p>
+         &copy; 2021 Abraham Kipsang & All rights Reserved .
+        </p>
+    </div>
+</body>
+</html>
